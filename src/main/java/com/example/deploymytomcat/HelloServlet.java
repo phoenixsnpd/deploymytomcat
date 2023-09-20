d8abc546a5e3bfaf60c8c036c49a7f9b38996882
phoenixsnpd <phoenix.snpd@gmail.com>
2023-09-20 15:35:23 +0300

    Ne commit

--- src/main/java/com/example/deploymytomcat/HelloServlet.java
+++ src/main/java/com/example/deploymytomcat/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "There's new Hello World!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
